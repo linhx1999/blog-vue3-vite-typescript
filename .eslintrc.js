a947d44f81956705d01db40c7fbf440cc9080a6b
Lin Huanxiang <linhx1999@163.com>
2022-06-10 11:43:26 +0800

    add Eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "vue-eslint-parser",
+  extends: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint", "prettier"],
+  rules: { "prettier/prettier": "error" },
+};
+
