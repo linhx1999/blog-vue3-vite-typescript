1c4ce680c266a2eb69f21cd76fef5ffa8063f6b4
linhx <linhx1999@163.com>
2022-06-14 10:37:59 +0800

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslint from "vite-plugin-eslint";
+
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint()]
-})
+  plugins: [vue(), eslint()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "src"),
+      },
+    ],
+  },
+});
