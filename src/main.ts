d7d770d7bbd542993da9c06455b1a9566619c873
linhx <linhx1999@163.com>
2022-06-14 16:25:09 +0800

    Revert "add elementplus and components vies"
    This reverts commit 691435649f826b63e19c7638f6bd1abfca023606.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "@/router/index";
 
 import "normalize.css";
-import { mockXHR } from "../mock/index";
 
-createApp(App).use(router).mount("#app");
-mockXHR();
+createApp(App).mount("#app");
--- src/settings.ts
+++
@@ -1,6 +0,0 @@
-const port: string = "3000";
-const title: string = "个人博客";
-const baseURL: string = `http://localhost:${port}`;
-const apiURL: string = `${baseURL}/blog_api`;
-
-export { title, baseURL, apiURL };
